f10dec41089344ccd89e453a395478489476b3b6
Peter Schwarz <peter.schwarz@bicycle.io>
2014-02-14 14:56:15 -0600

    added scm info

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
 (defproject clj-firmata "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://github.com/peterschwarz/clj-firmata"
+  :description "clj-firmata provides access to Standard Firmata (http://firmata.org/) commands via clojure"
+  :url "https://github.com/peterschwarz/clj-firmata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [clj-serial "1.0.0"]])
+                 [clj-serial "1.0.0"]]
+  :scm {:name "git"
+        :url "https://github.com/peterschwarz/clj-firmata"})
