b57f521bd0b99f210c6cc53883d8544787ac6222
Peter Schwarz <peter.schwarz@bicycle.io>
2014-09-04 15:29:02 -0500

    initial conversion to cljx

--- project.clj
+++ project.clj
@@ -3,9 +3,51 @@
   :url "https://github.com/peterschwarz/clj-firmata"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :jar-exclusions [#"\.cljx"]
+
+  :source-paths ["src/cljx"]
+
+  :test-paths ["target/test-classes"]
+
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2311"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
                  [clj-serial "2.0.2"]]
+
   :scm {:name "git"
         :url "https://github.com/peterschwarz/clj-firmata"}
-  :plugins [[lein-cloverage "1.0.2"]])
+
+  :profiles {:dev {:plugins [[com.keminglabs/cljx "0.4.0" 
+                                 :exclusions [org.clojure/clojure]]
+                             [lein-cloverage "1.0.2"]
+                             [lein-cljsbuild "1.0.3"]
+                             [com.cemerick/clojurescript.test "0.3.1"]]}}
+
+  :hooks [cljx.hooks]
+
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                 :output-path "target/classes"
+                 :rules :clj}
+
+                {:source-paths ["src/cljx"]
+                 :output-path "target/classes"
+                 :rules :cljs}
+
+                {:source-paths ["test/cljx"]
+                 :output-path "target/test-classes"
+                 :rules :clj}
+
+                {:source-paths ["test/cljx"]
+                 :output-path "target/test-classes"
+                 :rules :cljs}]}
+
+  :cljsbuild {:builds [{:id "test"
+                        :source-paths ["target/classes" "target/test-classes"]
+                        :compiler {:output-to "target/testable.js"
+                                   :output-dir "target/test-js"
+                                   :target :nodejs
+                                   :optimizations :simple
+                                   :hashbang false}}]
+              :test-commands {"unit-tests" ["node" :node-runner
+                                            "target/testable.js"]}})
