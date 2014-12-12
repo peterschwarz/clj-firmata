464d74e7137d8cb4d1f7a43885f3603861a292ce
Peter Schwarz <peter.schwarz@bicycle.io>
2014-12-12 14:16:34 -0600

    Updated dependencies

--- project.clj
+++ project.clj
@@ -15,8 +15,8 @@
   :jvm-opts ["-Xmx384m"]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2311"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [org.clojure/clojurescript "0.0-2411"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [clj-serial "2.0.2"]]
 
   :scm {:name "git"
@@ -25,16 +25,15 @@
   :profiles {:dev {:plugins [[com.keminglabs/cljx "0.5.0"]
                              [lein-cloverage "1.0.2"]
                              [lein-cljsbuild "1.0.3"]
-                             [com.cemerick/clojurescript.test "0.3.1"]
-                             [lein-npm "0.4.0"]
-                             [org.bodil/lein-noderepl "0.1.11"]]
+                             [com.cemerick/clojurescript.test "0.3.2"]
+                             [lein-npm "0.4.0"]]
                    :aliases {"cleantest" ["do" "clean," "cljx" "once," "test,"
                                           "npm" "install," "cljsbuild" "test"]
                              "cljstest" ["do" "cljx" "once," "cljsbuild" "test"]}}}
 
   :prep-tasks [["cljx" "once"]]
 
-  :node-dependencies [[serialport "1.4.6"]]
+  :node-dependencies [[serialport "1.4.9"]]
   :npm-root "target"
 
   :cljx {:builds [{:source-paths ["src/cljx"]
